1860cc6aff11699f4be801e7215f33febdb26bd8
sina mansoory <sinaman7979@gmail.com>
2022-09-01 17:18:10 +0430

    fix the hambergur menu bog & adding about page

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,19 +2,31 @@
 
 const hambergeur = document.querySelector(".hamberger-menue");
 const hambergeurLine = document.querySelector(".hamberger-menue").querySelectorAll(".line");
-console.log(hambergeur)
-hambergeur.addEventListener("click" , changelines);
+const navbar = document.querySelector('nav').querySelector('ul');
+const navItems = document.querySelector('nav').querySelector('ul').querySelectorAll('li');
+const headerImage = document.querySelector('nav').querySelector('.header-portrait');
 
+let setMenue = false
 
+hambergeur.addEventListener("click" , toggleMenu);
 
-function changelines() {
-    let status = true;
-    if(status) {
-        hambergeurLine.forEach(elem => elem.classList.toggle("cross"))
-        status = !status
-    }
-    else {
-        hambergeurLine.forEach(elem => elem.className = "line")
-    }
+function toggleMenu() {
+
+        if(!setMenue){
+                hambergeurLine.forEach(elem => elem.classList.add("cross"));
+                navbar.classList.add('show');
+                headerImage.classList.add('show');
+                navItems.forEach(items => items.classList.add('show'));
+                navbar.style.display = 'flex';
+                headerImage.style.display = 'flex';
+                setMenue = true;
+        }
+        else {
+            hambergeurLine.forEach(elem => elem.classList.remove("cross"));
+            navbar.classList.remove('show')
+            headerImage.classList.remove('show');
+            navItems.forEach(items => items.classList.remove('show'));
+            setMenue = false;
+        }
 }
 
